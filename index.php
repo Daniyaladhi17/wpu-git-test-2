b43799332adc681f87aca854a9b8c14a9a2f81f9
daniyaladhi17 <daniyal.adhi@gmail.com>
2022-02-16 08:16:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, veniam unde sint dolor explicabo, a, dolores aut voluptas magni cupiditate provident asperiores amet sunt sed excepturi culpa. Hic, dicta numquam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
